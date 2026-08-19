b75583c0bb93d4d620b3bfbbac1c48aa11cff853
Toprak <toprak17can@icloud.com>
2026-08-19 22:43:41 +0300

    katmandaki forward pass işlemlerindeki hata düzeltildi

--- katman.py
+++ katman.py
@@ -5,19 +5,19 @@ def sigmoid(x):
 
 
 
-input_activations = [0.2,0.5,0.3]
+input_activations = [0.25,0.5,0.3]
 weights = [
     [0.1,-0.15,0.2],
-    [0.2,0.4,-0.3],
+    [0.2,0.4,-0.3],  
     [1.4,-0.12,-1.1]
     ]
 biases = [1,-2,-0.7]
 output_activations = [0,0,0]
 
-for i,j in enumerate(input_activations): # Her nöron için
+for i,j in enumerate(output_activations):
     sum = int()
-    for k in weights[i]: 
-        sum+=j*k
-    sum+=biases[i]
-    output_activations[i]=sigmoid(sum)
+    for k,l in enumerate(input_activations):
+        sum+=weights[i][k]*l
+    output_activations[i]=sigmoid(sum+biases[i])
+
 print(output_activations)
