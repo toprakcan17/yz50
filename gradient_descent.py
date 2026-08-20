ff176e43415d417d178d94178ab4af4c89de5993
Toprak <toprak17can@icloud.com>
2026-08-20 16:51:34 +0300

    düzeltme: bias vektörünün güncellenmesi sağlandı

--- gradient_descent.py
+++ gradient_descent.py
@@ -8,7 +8,7 @@ weights = [
     [1.4,-0.12,-1.1]
     ]
 biases = [2,0,3]
-output_activations = [0,0,0]
+output_activations = list()
 truth_samples = [0,1,0]
 gradient = list()
 
@@ -27,7 +27,7 @@ def loss_function(output,truth):
             loss+=pow(j-truth[i], 2)
     return loss
 
-def numerical_derivative(i,j,weight):
+def numerical_derivative_neuron(i,j,weight):
     w = copy.deepcopy(weight)
     a = loss_function(compute_neural_network(input_activations,w,biases), truth_samples)
     k=0.00001
@@ -35,21 +35,37 @@ def numerical_derivative(i,j,weight):
     b = loss_function(compute_neural_network(input_activations,w,biases), truth_samples)
     return (b-a)/k
 
+def numerical_derivative_bias(i,weight):
+    bias = copy.deepcopy(biases)
+    a = loss_function(compute_neural_network(input_activations,weight,bias), truth_samples)
+    k=0.00001
+    bias[i]+=k
+    b = loss_function(compute_neural_network(input_activations,weight,bias), truth_samples)
+    return (b-a)/k
 
 def compute_gradient(w):
     gradient = list()
     for i,j in enumerate(w):  
         for k,_ in enumerate(j):
-            gradient.append(numerical_derivative(i,k,w))
+            gradient.append(numerical_derivative_neuron(i,k,w))
     return gradient
+
+def compute_bias_gradient(weights_new):
+    gradient = list()
+    for i in range(len(biases)):
+        gradient.append(numerical_derivative_bias(i, weights_new))
+    return gradient
+
 weights_new = copy.deepcopy(weights)
 
 for i in range(1,25):
     gradient = compute_gradient(weights_new)
+    bias_gradient = compute_bias_gradient(weights_new)
     step_size = 0.05
     for k,j in enumerate(gradient):
         weights_new[k//3][k%3] = weights_new[k//3][k%3]-step_size*j
-    for l in biases: l = l-step_size
+    for l,m in enumerate(bias_gradient): biases[l] = biases[l]-step_size*m
     loss = loss_function(compute_neural_network(input_activations,weights_new,biases), truth_samples)
     
-    print(loss)
+    print("{}. adım | Loss: {:.2f}".format(i, loss))
+print("Son weights matrisi {}\nSon bias vektörü {}".format(weights_new, biases))
