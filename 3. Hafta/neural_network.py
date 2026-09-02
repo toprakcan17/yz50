ada2d75f8851bc485928e02b8e9ccaf651c602a9
Toprak <toprak17can@icloud.com>
2026-09-02 15:21:32 +0300

    dorduncu gorev tamamlandi, neural network gradient descent ile egitilerek lossun onceki gorevdeki lossa yaklastigi gozlemlendi.

--- 3. Hafta/neural_network.py
+++ 3. Hafta/neural_network.py
@@ -22,19 +22,38 @@ for i in names:
         counts[encode_char(k)][encode_char(next_char)] += 1
 
 counts+=1
-print(counts.shape)
 normalised = counts.float() / counts.sum(1, keepdim=True)
 char_count = 0
-negative_log_likelihood = float()
 
-for i in names[:3]:
+xs = []
+ys = []
+
+for i in names:
     i = f'.{i}.'
-
     for j,k in enumerate(i[:-1]):
         next_char = i[j+1]
-        prob = normalised[encode_char(k),encode_char(next_char)]
-        negative_log_likelihood-=prob.log()
+        xs.append(encode_char(k))
+        ys.append(encode_char(next_char))
         char_count+=1
 
-average_nll = negative_log_likelihood/char_count
-print(negative_log_likelihood)
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+W = torch.randn(27,27, requires_grad=True)
+
+learning_step = 10
+step_count = 100
+losses = []
+for i in range(step_count):
+    x_one_hot = one_hot(xs, num_classes=27).float()
+    act = x_one_hot @ W
+    fake_counts = act.exp()
+    normalised_counts = fake_counts/fake_counts.sum(1, keepdim=True)
+    loss = -normalised_counts[torch.arange(len(ys)),ys].log().mean()
+    W.grad = None
+    loss.backward()
+    W.data -= W.grad * learning_step
+    losses.append(loss.data)
+    print(f'Step: {i+1} - Loss: {loss.data:.2f}')
+plt.plot(losses)
+plt.waitforbuttonpress()
