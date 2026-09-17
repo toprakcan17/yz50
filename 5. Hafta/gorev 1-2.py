cd347af09fd2b7e39e63611fc2318de3bdfd639c
Toprak Can <toprak@192.168.1.17>
2026-09-17 22:38:41 +0300

    ikinci gorev tamamlandi, elle butun gradientler hesaplandi

--- 5. Hafta/gorev 1-2.py
+++ 5. Hafta/gorev 1-2.py
@@ -23,6 +23,12 @@ def decode(index):
 def encode_str(string):
     return [encode(i) for i in string]
 
+def cmp(s, dt, t):
+  ex = torch.all(dt == t.grad).item()
+  app = torch.allclose(dt, t.grad)
+  maxdiff = (dt - t.grad).abs().max().item()
+  print(f'{s:15s} | exact: {str(ex):5s} | approximate: {str(app):5s} | maxdiff: {maxdiff}')
+
 xtr,ytr,xval,yval,xtest,ytest = list(), list(), list(), list(), list(), list()
 block_size = 3
 dims = 10
@@ -91,7 +97,12 @@ for step in range(step_count):
     emb_flattened_dev = emb_dev.view(batch_size, block_size*dims)
 
     h = ((emb_flattened @ W1) + B1)
-    bn_h = bn_gain * (h-h.mean(dim=0, keepdim=True))/torch.sqrt(h.var(dim=0, keepdim=True)) + bn_bias
+    var = h.var(dim=0, keepdim=True)
+    mean = h.mean(dim=0, keepdim=True)
+    sqrt_var = torch.sqrt(var)
+    inv_sqrt_var = sqrt_var**-1
+    diff = h-mean
+    bn_h = bn_gain * (diff * inv_sqrt_var) + bn_bias
     logits = bn_h.tanh() @ W2 + B2
     # loss = torch.nn.functional.cross_entropy(logits, ytr[idx])    
     logits_exp = logits.exp()
@@ -99,17 +110,59 @@ for step in range(step_count):
     probs = logits_exp * logits_exp_sum**-1
     logprobs = probs.log()
     loss = -logprobs[range(batch_size), ytr[idx]].mean() 
-    for i in [emb_flattened, emb, h, bn_h, logits, logits_exp, logits_exp_sum, probs, logprobs]: i.retain_grad()
+    for i in [emb_flattened, emb, h, bn_h, logits, logits_exp, logits_exp_sum, probs, logprobs, diff, inv_sqrt_var, sqrt_var, var, h, mean]: i.retain_grad()
+    for i in params: i.grad = None
     loss.backward()
+    if step==0:
+        grad_logprobs = torch.zeros_like(logprobs)
+        grad_logprobs[range(batch_size), ytr[idx]] = -batch_size**-1
+        grad_probs = grad_logprobs/probs
+        grad_logits_exp = logits_exp_sum**-1 * grad_probs
+        grad_counts_sum_inv = (logits_exp * grad_probs).sum(dim=1, keepdim=True)
+        grad_counts_sum = grad_counts_sum_inv*(-logits_exp_sum**-2)
+        grad_logits_exp += torch.ones_like(logits)*grad_counts_sum # broadcasting icin onemli
+        grad_logits = logits_exp*grad_logits_exp
+        grad_w2 = bn_h.tanh().T @ grad_logits
+        grad_b2 = grad_logits.sum(dim=0, keepdim=False)
+        grad_bn_h = grad_logits @ W2.T
+        grad_bn_h*= 1 - bn_h.tanh()**2
+        grad_diff = grad_bn_h * bn_gain * inv_sqrt_var
+        grad_inv_sqrt_var = (grad_bn_h*bn_gain*diff).sum(dim=0, keepdim=True)
+        grad_sqrt_var = grad_inv_sqrt_var*-sqrt_var**-2
+        grad_var = grad_sqrt_var * sqrt_var * 1/2 * var**-1
+        grad_mean = -grad_diff.sum(dim=0, keepdim=True)
+        print(mean.shape, diff.shape, h.shape)
+        grad_h = grad_diff + ((batch_size**-1)*grad_mean)*torch.ones_like(h)+grad_var * 2 * diff / (batch_size-1)
+        grad_w1 = emb_flattened.T @ grad_h
+        grad_b1 = grad_h.sum(dim=0, keepdim=False)
+        grad_emb_flattened = grad_h @ W1.T
+        grad_emb = grad_emb_flattened.view(batch_size, block_size, dims)
+        grad_embedding_matrix = torch.zeros_like(embedding_matrix)
+        for k in range(xtr[idx].shape[0]): 
+            for j in range(xtr[idx].shape[1]):
+                ix = xtr[idx][k,j]
+                grad_embedding_matrix[ix] += grad_emb[k, j]
 
-    grad_logprobs = torch.zeros_like(logprobs)
-    grad_logprobs[range(batch_size), ytr[idx]] = -batch_size**-1
-    grad_probs = grad_logprobs/probs
-    grad_logits_exp = logits_exp_sum**-1 * grad_probs
-    grad_counts_sum_inv = logits_exp * grad_probs
-    grad_counts_sum = grad_counts_sum_inv*(-logits_exp_sum**-2)
-    grad_logits_exp += torch.ones_like(logits)*grad_counts_sum # broadcasting icin onemli
-    grad_logits = logits_exp*grad_logits_exp
+        cmp('logprobs', grad_logprobs, logprobs)
+        cmp('probs', grad_probs, probs)
+        cmp('logits_exp', grad_logits_exp, logits_exp)
+        cmp('logits', grad_logits, logits)
+        cmp('w2', grad_w2, W2)
+        cmp('b2', grad_b2, B2)
+        cmp('bn_h', grad_bn_h, bn_h)
+        cmp('diff', grad_diff, diff)
+        cmp('inv_sqrt_var', grad_inv_sqrt_var, inv_sqrt_var)
+        cmp('sqrt_var', grad_sqrt_var, sqrt_var)
+        cmp('var', grad_var, var)
+        cmp('mean', grad_mean, mean)
+        cmp('h', grad_h, h)
+        cmp('w1', grad_w1, W1)
+        cmp('b1', grad_b1, B1)
+        cmp('emb_flattened', grad_emb_flattened, emb_flattened)
+        cmp('emb', grad_emb, emb)
+        cmp("embedding_matrix", grad_embedding_matrix, embedding_matrix)
+
+
 
 
     with torch.no_grad():
@@ -120,8 +173,7 @@ for step in range(step_count):
         bn_h = bn_gain * (h-bn_running_mean)/torch.sqrt(bn_running_var) + bn_bias
         logits_dev = bn_h.tanh() @ W2 + B2
         devloss = torch.nn.functional.cross_entropy(logits_dev, yval[idx_dev])
-    for i in params: i.grad = None
-    loss.backward()
+
     for i in params:
         i.data -= i.grad * 0.1
     #lr_stats.append(lr[step])
